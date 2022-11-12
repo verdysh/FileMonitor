a064d4c6c1cd356e6fbaaaa5e0296e9f01863ddd
ajoh504 <onegunnerunder@gmail.com>
2022-11-12 12:21:59 -0800

    feat: initial XAML setup

---
+++ VinPCGS/TextBlockDisplayControls.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinPCGS
+{
+    class TextBlockDisplayControls
+    {
+        /// <summary>
+        /// Display all currently stored file paths
+        /// </summary>
+        public void ShowAllFiles()
+        {
+
+        }
+
+        /// <summary>
+        /// Display file paths only if the files have changed since the last backup
+        /// </summary>
+        public void ShowRecentlyChangedFiles()
+        {
+
+        }
+    }
+}
