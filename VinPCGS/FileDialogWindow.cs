9e44bb96fc438343c0ee570dd717bd76a14d705a
ajoh504 <onegunnerunder@gmail.com>
2022-11-20 10:19:31 -0800

    refactor: add new class to handle the OpenFileDialog object

---
+++ VinPCGS/FileDialogWindow.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace VinPCGS
+{
+    internal class FileDialogWindow
+    {
+        /// <summary>
+        /// Open a FileDialog browser for the user to select a file.
+        /// </summary>
+        /// <returns> A string of the full file path </returns>
+        public string GetPath()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.ShowDialog();
+
+            if (openFileDialog.FileName != "")
+            {
+                return openFileDialog.FileName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
