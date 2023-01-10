86eeeaedd3f6479b7fbeeff3ea287515fc7439df
ajoh504 <onegunnerunder@gmail.com>
2023-01-10 07:18:54 -0800

    refactor: implement INotifyPropertyChanged interface

--- VinPCGS/TextBlockDisplay.cs
+++ VinPCGS/TextBlockDisplay.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace VinPCGS
 {
-    class TextBlockDisplay
+    class TextBlockDisplay : INotifyPropertyChanged
     {
+        // Private field to store all file paths
+        private string files; 
+
+        // Declare PropertyChanged event
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Public property to expose the private field
+        // Call OnPropertyChanged() whenever the files have changed.
+        public string Files
+        {
+            get { return files; }
+            set
+            {
+                files = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string files = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(files));
+        }
+
         /// <summary>
         /// Display all currently stored file paths
         /// </summary>
