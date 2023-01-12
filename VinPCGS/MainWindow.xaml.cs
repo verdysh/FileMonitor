dad1dcad2cdbd6f84876663405d363c56f6a3c25
ajoh504 <onegunnerunder@gmail.com>
2023-01-12 07:26:53 -0800

    fix: pass current instance of MainWindow
    current instance of MainWindow will be passed to the two method calls in order to update the XAML

--- VinPCGS/MainWindow.xaml.cs
+++ VinPCGS/MainWindow.xaml.cs
@@ -17,6 +17,10 @@ namespace VinPCGS
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Update JSON file, and update the Files property in textBlockDisplay in order to trigger
+        /// the PropertyChanged event
+        /// </summary>
         private void AddNewFile_Click(object sender, RoutedEventArgs e)
         {
             string newFile = FileDialogWindow.GetPath();
@@ -31,10 +35,11 @@ namespace VinPCGS
         /// <summary>
         /// Update XAML TextBlocks when the PropertyChanged event is fired
         /// </summary>
+        /// <remarks> Pass this MainWindow instance by reference to both method calls </remarks>
         private void TextBlockDisplay_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            textBlockDisplay.ShowAllFiles();
-            textBlockDisplay.ShowRecentlyChangedFiles();
+            textBlockDisplay.ShowAllFiles(this); 
+            textBlockDisplay.ShowRecentlyChangedFiles(this);
         }
 
         private void EditFiles_Click(object sender, RoutedEventArgs e)
