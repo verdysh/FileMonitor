f80401aec1a71c96f90d0650790b5368a9bbbf3d
ajoh504 <onegunnerunder@gmail.com>
2023-01-16 05:59:09 -0800

    refactor: write SQL commands

--- FileMonitor/Database/DatabaseBuilder.cs
+++ FileMonitor/Database/DatabaseBuilder.cs
@@ -12,6 +12,15 @@ namespace FileMonitor.Database
 
         public void Create()
         {
+            string[] SQLCommands = new string[]
+            {
+                "CREATE TABLE source_file (id INT, path VARCHAR(260))",
+                "CREATE TABLE backup_file (id INT, path VARCHAR(260))",
+                "CREATE TABLE source_backup_relational (source_file_id INT, backup_file_id INT)",
+                "CREATE TABLE source_hash (id INT, hashcode VARCHAR(160))",
+                "CREATE TABLE backup_hash (id INT, hashcode VARCHAR(160))",
+                "CREATE TABLE hash_relational (source_hash_id INT, backup_hash_id INT)"
+            };
             SQLiteConnection.CreateFile(path);
             SQLiteConnection SQLconnection = new SQLiteConnection("Data Source=FMDB.sqllite;Version=3");
             SQLconnection.Open();
