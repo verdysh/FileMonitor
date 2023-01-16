843cfdf4b0ffbf06beedc79f0c0269a735fb7954
ajoh504 <onegunnerunder@gmail.com>
2023-01-16 05:42:35 -0800

    refactor: create database builder class

---
+++ FileMonitor/Database/DatabaseBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SQLite;
+
+namespace FileMonitor.Database
+{
+    internal class DatabaseBuilder
+    {
+        private string path;
+        public DatabaseBuilder(string path) 
+        {
+            this.path = path;
+        }
+
+        public void Create()
+        {
+            SQLiteConnection.CreateFile(path);
+            SQLiteConnection SQLconnection = new SQLiteConnection("Data Source=FMDB.sqllite;Version=3");
+            SQLconnection.Open();
+        }
+    }
+}
