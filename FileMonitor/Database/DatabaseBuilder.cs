097351888aa709cd30df7ae5c9645c2f91d55c74
ajoh504 <onegunnerunder@gmail.com>
2023-01-20 06:33:26 -0800

    refactor: add loop to run all "CREATE TABLE" commands

--- FileMonitor/Database/DatabaseBuilder.cs
+++ FileMonitor/Database/DatabaseBuilder.cs
@@ -16,14 +16,22 @@ namespace FileMonitor.Database
             {
                 "CREATE TABLE source_file (id INT, path VARCHAR(260))",
                 "CREATE TABLE backup_file (id INT, path VARCHAR(260))",
-                "CREATE TABLE source_backup_relational (source_file_id INT, backup_file_id INT)",
+                "CREATE TABLE source_backup_file_rel (source_file_id INT, backup_file_id INT)",
                 "CREATE TABLE source_hash (id INT, hashcode VARCHAR(160))",
                 "CREATE TABLE backup_hash (id INT, hashcode VARCHAR(160))",
-                "CREATE TABLE hash_relational (source_hash_id INT, backup_hash_id INT)"
+                "CREATE TABLE source_backup_hash_rel (source_hash_id INT, backup_hash_id INT)",
+                "CREATE TABLE source_file_hash_rel (source_file_id INT, source_hash_id VARCHAR(160))",
+                "CREATE TABLE backup_file_hash_rel (backup_file_id INT, backup_hash_id VARCHAR(160))"
             };
             SQLiteConnection.CreateFile(path);
             SQLiteConnection SQLconnection = new SQLiteConnection("Data Source=FMDB.sqllite;Version=3");
             SQLconnection.Open();
+
+            for (int i = 0; i < SQLCommands.Length; i++)
+            {
+                SQLiteCommand command = new SQLiteCommand(SQLCommands[i], SQLconnection);
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
