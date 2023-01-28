75ecf10a4e584f361a654b3460d00733b1247f9c
ajoh504 <onegunnerunder@gmail.com>
2023-01-28 07:26:00 -0800

    refactor: add IDQuery class

---
+++ FileMonitor/Database/IDQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMonitor.Database
+{
+    internal class IDQuery
+    {
+        /// <summary>
+        /// Get IDs from database where only one column contains IDs
+        /// </summary>
+        /// <param name="table"> Table to retrieve IDs from </param>
+        /// <returns> An integer list of all table IDs </returns>
+        public List<string> GetSingleColumnIDs(string table)
+        {
+            List<string> result = new List<string>();
+            string query = $"SELECT id FROM {table}";
+
+            SQLiteConnection connection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+            connection.Open();
+
+            SQLiteCommand command = new SQLiteCommand(query, connection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read()) result.Add(reader["id"].ToString());
+            connection.Close();
+            return result;
+        }
+    }
+}
