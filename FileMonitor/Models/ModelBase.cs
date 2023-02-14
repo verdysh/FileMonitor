78d6037c8938d6a42500bddbc0f9835e3eb87b15
ajoh504 <onegunnerunder@gmail.com>
2023-02-14 06:30:44 -0800

    refactor: create a view model base class - implement INotifyPropertyChanged

---
+++ FileMonitor/Models/ModelBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMonitor.Models
+{
+    /// <summary>
+    /// Provides a base class for all view model classes
+    /// </summary>
+    internal abstract class ModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T field, T newValue, [CallerMemberName] string? propertyName = null)
+        {
+            if (!field.Equals(newValue))
+            {
+                field = newValue;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ FileMonitor/Models/SourceFileView.cs
@@ -0,0 +1,20 @@
+﻿using FileMonitor.Database;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace FileMonitor.Models 
+{
+    /// <summary>
+    /// Defines a view model class for accessing a list of files that are monitored by the program.
+    /// </summary>
+    class SourceFileView : ModelBase
+    {
+        private ObservableCollection<string> allFiles;
+
+        public SourceFileView() 
+        {
+        }
+    }
+}
