d131661a9af8e0508ca774cf3f4ed78288430a96
ajoh504 <onegunnerunder@gmail.com>
2023-03-01 06:12:11 -0800

    fix: rowid is generated but not used - rewrite SQL statement to treat the id column as the primary key for each table - source: https://www.sqlitetutorial.net/sqlite-autoincrement/

--- FileMonitor/Database/DatabaseBuilder.cs
+++ FileMonitor/Database/DatabaseBuilder.cs
@@ -12,14 +12,14 @@ namespace FileMonitor.Database
         /// </summary>
         private readonly string[] commands = new string[]
         {
-            "CREATE TABLE source_file (id INT, path VARCHAR(260))",
-            "CREATE TABLE backup_file (id INT, path VARCHAR(260))",
-            "CREATE TABLE source_backup_file_rel (source_file_id INT, backup_file_id INT)",
-            "CREATE TABLE source_hash (id INT, hash_code VARCHAR(160))",
-            "CREATE TABLE backup_hash (id INT, hash_code VARCHAR(160))",
-            "CREATE TABLE source_backup_hash_rel (source_hash_id INT, backup_hash_id INT)",
-            "CREATE TABLE source_file_hash_rel (source_file_id INT, source_hash_id INT)",
-            "CREATE TABLE backup_file_hash_rel (backup_file_id INT, backup_hash_id INT)"
+            "CREATE TABLE source_file (id INT PRIMARY KEY, path VARCHAR(260))",
+            "CREATE TABLE backup_file (id INT PRIMARY KEY, path VARCHAR(260))",
+            "CREATE TABLE source_backup_file_rel (id INT PRIMARY KEY, source_file_id INT, backup_file_id INT)",
+            "CREATE TABLE source_hash (id INT PRIMARY KEY, hash_code VARCHAR(160))",
+            "CREATE TABLE backup_hash (id INT PRIMARY KEY, hash_code VARCHAR(160))",
+            "CREATE TABLE source_backup_hash_rel (id INT PRIMARY KEY, source_hash_id INT, backup_hash_id INT)",
+            "CREATE TABLE source_file_hash_rel (id INT PRIMARY KEY, source_file_id INT, source_hash_id INT)",
+            "CREATE TABLE backup_file_hash_rel (id INT PRIMARY KEY, backup_file_id INT, backup_hash_id INT)"
         };
 
         /// <summary>
