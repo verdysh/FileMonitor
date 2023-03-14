dc147db2acd328bf698ad3189f3dc41b66a9d417
ajoh504 <onegunnerunder@gmail.com>
2023-03-14 06:09:54 -0700

    feat: add class to handle performing a backup

---
+++ FileMonitor/Models/Backup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+
+namespace FileMonitor.Models
+{
+    internal class Backup
+    {
+        private DriveInfo[] allLogicalDrives;
+        public DriveInfo[] AllLogicalDrives
+        {
+            get => allLogicalDrives;
+        }
+
+        public Backup()
+        {
+            allLogicalDrives = DriveInfo.GetDrives();
+        }
+
+        public static void Run(ReadOnlyObservableCollection<string> files)
+        {
+            
+        }
+    }
+}
