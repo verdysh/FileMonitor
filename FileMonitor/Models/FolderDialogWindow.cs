808a34fd517291fbaa665575748f40f897dd7b29
ajoh504 <onegunnerunder@gmail.com>
2023-03-14 06:10:21 -0700

    feat: add class to act as a folder dialog browser

---
+++ FileMonitor/Models/FolderDialogWindow.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace FileMonitor.Models
+{
+    class FolderDialogWindow
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPath()
+        {
+            return "";
+        }
+    }
+}
