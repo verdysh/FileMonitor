e55518cbb13522627fe0d5c31facadb651ba18c8
Alex <onegunnerunder@gmail.com>
2023-04-22 13:23:48 -0700

    fixed error where the wrong package was installed - installed EntityFrameworkCore.Sqlite.Core

---
+++ DataAccessLayer/Entities/SourceFile.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entities
+{
+    /// <summary>
+    /// An Entity class for the source_file table
+    /// </summary>
+    [Table("source_file")]
+    public class SourceFile
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+    }
+}
--- DataAccessLayer/FileMonitorDbContext.cs
+++ DataAccessLayer/FileMonitorDbContext.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer
 {
+    /// <summary>
+    /// A DbContext class for accessing the database
+    /// </summary>
     public class FileMonitorDbContext : DbContext
     {
+        private readonly string _connectionString;
+        public DbSet<SourceFile> SourceFiles { get; set; }
 
+        public FileMonitorDbContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(_connectionString);
+        }
     }
 }
