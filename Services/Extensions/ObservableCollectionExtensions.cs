1967a3ec2ba0e3379f6448aa0ef44a2b5e73821b
ajoh504 <onegunnerunder@gmail.com>
2023-05-01 17:50:23 -0700

    Add extension method to remove a range of items from an ObservableCollection

---
+++ Services/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Services.Extensions
+{
+    public static class ObservableCollectionExtensions
+    {
+        public static void RemoveRange<T>(this ObservableCollection<T> collection, IEnumerable<T> itemsToRemove)
+        {
+            foreach (var item in itemsToRemove)
+            {
+                collection.Remove(item);
+            }
+        }
+    }
+}
