bcb54ef50c749ec3613fc8e2c3beec92ebb9012a
ajoh504 <onegunnerunder@gmail.com>
2023-05-01 17:27:25 -0700

    Update view model to implement ObservableCollection - remove INotifyPropertyChanged since ObservableCollection implements INotifyCollectionChanged

--- FileMonitor/ViewModels/FilesViewModel.cs
+++ FileMonitor/ViewModels/FilesViewModel.cs
@@ -1,26 +1,18 @@
 ﻿using Services.SourceFiles.Dto;
-using System.Collections.Generic;
-using System.ComponentModel;
+using System.Collections.ObjectModel;
 
 namespace FileMonitor.ViewModels
 {
-    internal class FilesViewModel : INotifyPropertyChanged
+    internal class FilesViewModel 
     {
-        private List<SourceFileDto> _files;
-        public List<SourceFileDto> Files
+        private ObservableCollection<SourceFileDto> _files;
+        public ObservableCollection<SourceFileDto> Files
         {
             get { return _files; } 
             set
             {
                 _files = value;
-                OnPropertyChanged(nameof(Files));
             }
-        }
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
