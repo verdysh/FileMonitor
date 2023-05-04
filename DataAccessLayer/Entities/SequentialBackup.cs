199239c7c5b4846c40b5c139056891179dd79122
ajoh504 <onegunnerunder@gmail.com>
2023-05-04 15:51:25 -0700

    Add entity class to store file paths for sequential backups

---
+++ DataAccessLayer/Entities/SequentialBackup.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entities
+{
+    /// <summary>
+    /// An entity class for the sequential_backup table
+    /// </summary>
+    [Table("sequential_backup")]
+    public class SequentialBackup
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+    }
+}
