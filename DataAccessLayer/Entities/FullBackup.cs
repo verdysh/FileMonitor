999c4702583fe0d50f4af203dc7b2724580ade27
ajoh504 <onegunnerunder@gmail.com>
2023-05-04 15:51:10 -0700

    Add entity class to store file paths for full backups

---
+++ DataAccessLayer/Entities/FullBackup.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entities
+{
+    /// <summary>
+    /// An entity class for the full_backup table
+    /// </summary>
+    [Table("full_backup")]
+    public class FullBackup
+    {
+        public int Id { get; set; }
+        public string Path { get; set; }
+    }
+}
