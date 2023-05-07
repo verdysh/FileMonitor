6d5c9bed876289ec23f732d32f5553831c4cfcb8
ajoh504 <onegunnerunder@gmail.com>
2023-05-07 10:33:24 -0700

    Rename SequentialBackup file - Update database table name and class name

--- DataAccessLayer/Entities/SequentialBackupPath.cs
+++ DataAccessLayer/Entities/SequentialBackupPath.cs
@@ -5,8 +5,8 @@ namespace DataAccessLayer.Entities
     /// <summary>
     /// An entity class for the sequential_backup table
     /// </summary>
-    [Table("sequential_backup")]
-    public class SequentialBackup
+    [Table("sequential_backup_path")]
+    public class SequentialBackupPath
     {
         public int Id { get; set; }
         public string Path { get; set; }
