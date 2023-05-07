bca56fe137eaaa1b0303726ef214b367f352de90
ajoh504 <onegunnerunder@gmail.com>
2023-05-07 10:33:07 -0700

    Rename FullBackup file - update database table name and class name

--- DataAccessLayer/Entities/FullBackupPath.cs
+++ DataAccessLayer/Entities/FullBackupPath.cs
@@ -5,8 +5,8 @@ namespace DataAccessLayer.Entities
     /// <summary>
     /// An entity class for the full_backup table
     /// </summary>
-    [Table("full_backup")]
-    public class FullBackup
+    [Table("full_backup_path")]
+    public class FullBackupPath
     {
         public int Id { get; set; }
         public string Path { get; set; }
