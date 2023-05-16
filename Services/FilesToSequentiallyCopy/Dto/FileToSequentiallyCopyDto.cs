3ccc823424942161c693b3a2845b273dfd0ae4c9
ajoh504 <onegunnerunder@gmail.com>
2023-05-16 06:06:43 -0700

    Add new service and dto object for files files that should be copied sequentially

---
+++ Services/FilesToSequentiallyCopy/Dto/FileToSequentiallyCopyDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.FilesToSequentiallyCopy.Dto
+{
+    public class FileToSequentiallyCopyDto
+    {
+        public int Id { get; set; }
+        public int SourcFileId { get; set; }
+        public string Path { get; set; }
+    }
+}
