694d4b601cc760b38512384d5cd608df2b473ffc
ajoh504 <onegunnerunder@gmail.com>
2023-05-18 18:15:37 -0700

    Add SourceFileRepository for accessing SourceFile entities

---
+++ DataAccessLayer/Repositories/SourceFileRepository.cs
@@ -0,0 +1,12 @@
+﻿using DataAccessLayer.Entities;
+
+
+namespace DataAccessLayer.Repositories
+{
+    public class SourceFileRepository : RepositoryBase<SourceFile>, ISourceFileRepository
+    {
+        public SourceFileRepository(FileMonitorDbContext db) : base(db)
+        {
+        }
+    }
+}
