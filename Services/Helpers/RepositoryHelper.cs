18c56a656b4f6e7d4e20b5e31c8bc30d959a8e28
ajoh504 <onegunnerunder@gmail.com>
2023-05-20 08:51:58 -0700

    Add RepositoryHelper to provide an instance of the SourceFileRepository

---
+++ Services/Helpers/RepositoryHelper.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Repositories;
+using System.Configuration;
+
+namespace Services.Helpers
+{
+    internal class RepositoryHelper
+    {
+        public static ISourceFileRepository CreateSourceFileRepositoryInstance()
+            => new SourceFileRepository(CreateInstance());
+
+        private static FileMonitorDbContext CreateInstance()
+            => new FileMonitorDbContext(ConfigurationManager.ConnectionStrings[nameof(FileMonitorDbContext)].ConnectionString);
+    }
+}
