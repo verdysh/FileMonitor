331941224408d080036cd93b59d89d854861616d
ajoh504 <onegunnerunder@gmail.com>
2023-05-20 09:31:24 -0700

    Add FullBackup repository

---
+++ DataAccessLayer/Repositories/FullBackupPathRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repositories
+{
+    public class FullBackupPathRepository: RepositoryBase<FullBackupPath>, IFullBackupPathRepository
+    {
+        public FullBackupPathRepository(FileMonitorDbContext db) : base(db)
+        {
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/IFullBackupPathRepository.cs
@@ -0,0 +1,8 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repositories
+{
+    public interface IFullBackupPathRepository: IRepository<FullBackupPath>
+    {
+    }
+}
