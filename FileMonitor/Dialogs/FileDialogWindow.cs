e25c7529729a1b306c593a2ee583404553c0be8a
ajoh504 <onegunnerunder@gmail.com>
2023-05-22 06:03:00 -0700

    Update logic to handle getting multiple files

--- FileMonitor/Dialogs/FileDialogWindow.cs
+++ FileMonitor/Dialogs/FileDialogWindow.cs
@@ -8,12 +8,12 @@ namespace FileMonitor.Dialogs
         /// Open a FileDialog browser for the user to select a file.
         /// </summary>
         /// <returns> A string of the full file path </returns>
-        public static string GetPath()
+        public static string[] GetPath()
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Multiselect = true;
             openFileDialog.ShowDialog();
-            return openFileDialog.FileName;
+            return openFileDialog.FileNames;
         }
     }
 }
