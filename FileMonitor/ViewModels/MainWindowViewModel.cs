eea62f23cb679d3d83a90956e6e1fd999be04978
ajoh504 <onegunnerunder@gmail.com>
2023-05-23 06:04:22 -0700

    Implement single view model - Set width properties for columns - Subscribe to collection changed event

---
+++ FileMonitor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,106 @@
+﻿using Services.Dto;
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
+namespace FileMonitor.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<FullBackupDto> _backupPaths;
+        private ObservableCollection<SourceFileDto> _files;
+        private bool _backupSelected;
+        private int _backupWidth;
+        private int _fileWidth;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public bool BackupSelected
+        {
+            get { return _backupSelected; }
+            set
+            {
+                _backupSelected = value;
+                OnPropertyChanged(nameof(BackupSelected));
+            }
+        }
+
+        public int BackupWidth
+        {
+            get { return _backupWidth; }
+            set
+            {
+                _backupWidth = value * 7;
+                OnPropertyChanged(nameof(BackupWidth));
+            }
+        }
+
+        public ObservableCollection<FullBackupDto> BackupPaths
+        {
+            get { return _backupPaths; }
+            set
+            {
+                _backupPaths = value;
+            }
+        }
+
+        public ObservableCollection<SourceFileDto> Files
+        {
+            get { return _files; }
+            set
+            {
+                _files = value;
+            }
+        }
+
+        public int FileWidth
+        {
+            get { return _fileWidth; }
+            set
+            {
+                _fileWidth = value;
+
+            }
+        }
+
+        public MainWindowViewModel(ObservableCollection<FullBackupDto> backups, ObservableCollection<SourceFileDto> sourceFiles)
+        {
+            _backupPaths = backups;
+            _files = sourceFiles;
+            _backupSelected = IsAnyBackupSelected();
+            BackupPaths.CollectionChanged += (sender, e) 
+                => BackupWidth = GetColumnWidth(_backupPaths);
+            Files.CollectionChanged += (sender, e)
+                => FileWidth = GetColumnWidth(_files);
+        }
+
+        public void OnPropertyChanged(string propertyName) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool IsAnyBackupSelected()
+        {
+            foreach (FullBackupDto dto in  _backupPaths)
+            {
+                if (dto.IsSelected == true) return true;
+            }
+            return false;
+        }
+
+        public int GetColumnWidth<T>(ObservableCollection<T> collection) where T : IHasPath
+        {
+            int maxWidth = 1;
+            foreach (T dto in collection)
+                maxWidth = Math.Max(maxWidth, dto.Path.Length);
+            return maxWidth;
+        }
+
+        public void Init()
+        {
+            BackupWidth = GetColumnWidth(_backupPaths);
+            FileWidth = GetColumnWidth(_files);
+        }
+    }
+}
