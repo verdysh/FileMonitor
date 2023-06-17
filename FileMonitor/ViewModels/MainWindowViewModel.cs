2babde9ac13d525dcbe8015e525809238afa3294
ajoh504 <onegunnerunder@gmail.com>
2023-06-17 09:26:59 -0700

    Remove bloat - The "Width" property code can be removed because the XAML GridViewColumn width is now bound to the width of the ListView

--- FileMonitor/ViewModels/MainWindowViewModel.cs
+++ FileMonitor/ViewModels/MainWindowViewModel.cs
@@ -11,8 +11,6 @@ namespace FileMonitor.ViewModels
         private ObservableCollection<BackupPathDto> _backupPaths;
         private ObservableCollection<SourceFileDto> _files;
         private bool _backupSelected;
-        private int _backupWidth;
-        private int _fileWidth;
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
@@ -23,26 +21,6 @@ namespace FileMonitor.ViewModels
             {
                 _backupSelected = value;
                 OnPropertyChanged(nameof(BackupSelected));
-            }
-        }
-
-        public int BackupWidth
-        {
-            get { return _backupWidth; }
-            set
-            {
-                _backupWidth = value * 7;
-                OnPropertyChanged(nameof(BackupWidth));
-            }
-        }
-
-        public int FileWidth
-        {
-            get { return _fileWidth; }
-            set 
-            { 
-                _fileWidth = value * 7;
-                OnPropertyChanged(nameof(FileWidth));
             }
         }
 
@@ -63,10 +41,6 @@ namespace FileMonitor.ViewModels
             _backupPaths = backups;
             _files = sourceFiles;
             _backupSelected = IsAnyBackupSelected();
-            BackupPaths.CollectionChanged += (sender, e) 
-                => BackupWidth = GetColumnWidth(_backupPaths);
-            SourceFiles.CollectionChanged += (sender, e)
-                => FileWidth = GetColumnWidth(_files);
         }
 
         public void OnPropertyChanged(string propertyName) 
@@ -81,20 +55,6 @@ namespace FileMonitor.ViewModels
                 if (dto.IsSelected == true) return true;
             }
             return false;
-        }
-
-        public int GetColumnWidth<T>(ObservableCollection<T> collection) where T : IHasPath
-        {
-            int maxWidth = 1;
-            foreach (T dto in collection)
-                maxWidth = Math.Max(maxWidth, dto.Path.Length);
-            return maxWidth;
-        }
-
-        public void Init()
-        {
-            BackupWidth = GetColumnWidth(_backupPaths);
-            FileWidth = GetColumnWidth(_files);
         }
     }
 }
