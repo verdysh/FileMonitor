5e3fe2409826ff977f5c8ca59a695b4503cd5dd5
ajoh504 <onegunnerunder@gmail.com>
2023-06-17 10:03:52 -0700

    Remove unnecessary class

--- Services/Dto/FileToSequentiallyCopyDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Services.Dto
-{
-    public class FileToSequentiallyCopyDto
-    {
-        public int Id { get; set; }
-        public int SourcFileId { get; set; }
-        public string Path { get; set; }
-    }
-}
