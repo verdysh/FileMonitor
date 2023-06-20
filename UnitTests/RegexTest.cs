a0243ced47ac6b035b0f76cf36194b0d8e4a1d6f
ajoh504 <onegunnerunder@gmail.com>
2023-06-20 05:57:01 -0700

    Add UnitTests project

---
+++ UnitTests/RegexTest.cs
@@ -0,0 +1,15 @@
+using DataAccessLayer.Entities;
+using FileMonitor.FileBackups;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class RegexTest
+    {
+        [TestMethod]
+        public void TestIncrementingFileNames()
+        {
+            //Backup backup = new Backup();
+        }
+    }
+}
