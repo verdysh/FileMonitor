a1c86f6f75898808706b61c7bd3b4003f156af10
ajoh504 <onegunnerunder@gmail.com>
2023-06-27 05:38:56 -0700

    Update code comments

--- Services/Helpers/EncryptionHelper.cs
+++ Services/Helpers/EncryptionHelper.cs
@@ -1,9 +1,21 @@
 ﻿using System.Security.Cryptography;
+using DataAccessLayer.Entities;
 
 namespace Services.Helpers
 {
+    /// <summary>
+    /// A static helper class for generating a <see cref="SHA1"/> hash. The hash is stored as a database record with the <see cref="SourceFile"/> Entity. 
+    /// </summary>
+    /// <remarks>
+    /// The program compares the stored hash to the current hash to determine if the file has changed since the last time it was copied to a backup location.
+    /// </remarks>
     public static class EncryptionHelper
     {
+        /// <summary>
+        /// Get a <see cref="SHA1"/> hash for the given file.
+        /// </summary>
+        /// <param name="file"> A fully qualified file path to retrieve a hash for. </param>
+        /// <returns> A <see cref="SHA1"/> hash for the provided file. </returns>
         public static string GetHash(string file)
         {
             using var sha1 = SHA1.Create();
