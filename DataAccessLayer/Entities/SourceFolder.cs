0baf5018bc9be62d2efec377f82f429903e8d3ea
ajoh504 <onegunnerunder@gmail.com>
2023-06-30 05:41:59 -0700

    Add `SourceFolder` Entity - This Entity stores the full path to any folder that is monitored by the program

---
+++ DataAccessLayer/Entities/SourceFolder.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entities
+{
+    /// <summary>
+    /// An Entity class for the source_folder table. These records store paths to all folders being monitored by the program.
+    /// </summary>
+    [Table("source_folder")]
+    public class SourceFolder
+    {
+        /// <summary>
+        /// The database primary key. 
+        /// </summary>
+        /// <remarks>
+        /// Use to remove Entities from the database.
+        /// </remarks>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The full path to where the folder is stored.
+        /// </summary>
+        public string? Path { get; set; }
+    }
+}
