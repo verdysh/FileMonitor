089d5323df8b61b398b182506e6338398e464c1d
ajoh504 <onegunnerunder@gmail.com>
2023-07-01 05:20:23 -0700

    Add `FolderFileMappingRepository`

---
+++ DataAccessLayer/Repositories/FolderFileMappingRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repositories
+{
+    /// <summary>
+    /// A repository for the FolderFileMapping Entity. This class inherits from <see cref="RepositoryBase{TEntity}"/> and <see cref="IFolderFileMapping"/>.
+    /// </summary>
+    public class FolderFileMappingRepository : RepositoryBase<FolderFileMapping>, IFolderFileMapping
+    {
+        /// <summary>
+        /// Defines the <see cref="FolderFileMappingRepository"/> class constructor.
+        /// </summary>
+        /// <param name="db"> The database context object of type <see cref="FileMonitorDbContext"/>. Provides access to the Entity Framework API. This parameter is passed to the <c>base()</c> constructor of the <see cref="RepositoryBase{TEntity}"/> class. </param>
+        public FolderFileMappingRepository(FileMonitorDbContext db) : base(db)
+        {
+        }
+    }
+}
