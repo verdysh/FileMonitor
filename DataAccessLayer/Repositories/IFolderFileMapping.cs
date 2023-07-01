d6bf444642b93c1ba161320dac1d3fde464acf02
ajoh504 <onegunnerunder@gmail.com>
2023-07-01 05:20:36 -0700

    Add `IFolderFileMapping`

---
+++ DataAccessLayer/Repositories/IFolderFileMapping.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repositories
+{
+    /// <summary>
+    /// A repository interface for the FolderFileMapping Entity.
+    /// </summary>
+    public interface IFolderFileMapping : IRepository<FolderFileMapping>
+    {
+    }
+}
