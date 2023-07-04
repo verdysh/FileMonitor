a45fca5170b7467ca91ce8e74d9fdc67a74bf5aa
ajoh504 <onegunnerunder@gmail.com>
2023-07-04 12:27:29 -0700

    Add SourceFolderDto - This data transfer object will be used to represent any folders that are being monitored by the program.

---
+++ Services/Dto/SourceFolderDto.cs
@@ -0,0 +1,26 @@
+﻿namespace Services.Dto
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SourceFolderDto
+    {
+        /// <summary>
+        /// The database primary key. 
+        /// </summary>
+        /// <remarks>
+        /// Use to remove Entities from the database.
+        /// </remarks>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The full file path to where the folder is stored.
+        /// </summary>
+        public string? Path { get; set; }
+
+        /// <summary>
+        /// A collection of all file paths that are within this folder, including any subdirectories of the folder. The files are represented as data transfer objects.
+        /// </summary>
+        public IEnumerable<SourceFileDto>? SourceFiles { get; set; }
+    }
+}
