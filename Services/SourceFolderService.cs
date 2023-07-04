6479981362beac326f060fef3bcb1786be8f7832
ajoh504 <onegunnerunder@gmail.com>
2023-07-04 12:29:49 -0700

    Add public method: `FoldersAreUpdated` - This method returns true if any monitored folder contains newly added files - If returning true, the method provides an out parameter, represented as a key/value pair where the keys are SourceFolderDto objects and the values are lists of new file paths.

--- Services/SourceFolderService.cs
+++ Services/SourceFolderService.cs
@@ -1,5 +1,6 @@
 ﻿using DataAccessLayer.Entities;
 using DataAccessLayer.Repositories;
+using Services.Dto;
 
 namespace Services
 {
@@ -89,17 +90,64 @@ namespace Services
             _sourceFolderRepository.SaveChanges();
         }
 
-
-        public void CheckFoldersForNewFiles(IEnumerable<string> folders)
+        /// <summary>
+        /// Returns true if any monitored folder contains newly added files, false otherwise. 
+        /// </summary>
+        /// <param name="newFilesFromMonitoredFolder"> A <see cref="Dictionary{TKey, TValue}"/> using <see cref="SourceFolderDto"/> objects for keys, and a list of strings for values. The values represent all files that have been added to the monitored folder. </param>
+        public bool FoldersAreUpdated(
+            out Dictionary<SourceFolderDto, List<string>>? newFilesFromMonitoredFolder)
         {
-            List<SourceFolder> sourceFolders = _sourceFolderRepository.GetRange(f => folders.Contains(f.Path));
-            foreach (SourceFolder folder in sourceFolders)
-            {
+            List<SourceFolderDto> folders = _sourceFolderRepository.GetRange(
+                f => true,
+                f => new SourceFolderDto
+                {
+                    Id = f.Id,
+                    Path = f.Path
+                },
+                f => f.Id);
 
+            bool foldersAreUpdated = false;
+            newFilesFromMonitoredFolder = null;
+            foreach (SourceFolderDto folder in folders)
+            {
+                string[] currentFiles = GetCurrentFiles(folder);
+                List<string> storedFiles = GetStoredFiles(folder);
+                foreach(string file in currentFiles)
+                {
+                    if (storedFiles.Contains(file)) continue;
+                    else
+                    {
+                        foldersAreUpdated = true;
+                        if(!newFilesFromMonitoredFolder.TryAdd(folder, new List<string>() { file }))
+                            newFilesFromMonitoredFolder[folder].Add(file);
+                    }
+                }
             }
+            return foldersAreUpdated;
         }
 
-        //private List<FolderFileMapping> GetFolderFileMappings() { }
+        // Get all current files and (including subdirectories) from the provided folder.
+        private string[] GetCurrentFiles(SourceFolderDto folder) 
+            => Directory.GetFileSystemEntries(folder.Path, "*", SearchOption.AllDirectories);
+
+        // Get all files stored in the database if they are mapped to a SourceFolder object. 
+        private List<string> GetStoredFiles(SourceFolderDto folder)
+        {
+            List<string> result = new List<string>();
+            List<FolderFileMapping> mapping = _folderFileMappingRepository.GetRange(
+                ffm => ffm.SourceFolderId == folder.Id);
+            foreach (FolderFileMapping map in mapping)
+            {
+                SourceFileDto? file = _sourceFileRepository.FirstOrDefault(
+                    f => f.Id == map.Id,
+                    f => new SourceFileDto
+                    {
+                        Path = f.Path
+                    });
+                if(file != null) result.Add(file.Path);
+            }
+            return result;
+        }
 
         /// <summary>
         /// Ensures that the service objects are properly disposed. Also calls <c>Dispose</c> on the repository objects.
