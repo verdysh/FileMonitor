8ecc634c666b2cc6777e4dd26662b934bb7f704b
ajoh504 <onegunnerunder@gmail.com>
2023-07-11 06:12:33 -0700

    Code organization - Shorten the FilesAddedToFolders method by moving logic into several private methods

--- Services/SourceFolderService.cs
+++ Services/SourceFolderService.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLayer.Entities;
 using DataAccessLayer.Repositories;
 using Services.Dto;
+using Services.Helpers;
 
 namespace Services
 {
@@ -46,117 +47,6 @@ namespace Services
         }
 
         /// <summary>
-        /// Adds a monitored folder to the database. This method ensures appropriate mapping from the source folder to all child files contained within it. <see cref="SourceFileService.Add(string)"/> must be called first on all new file paths before this method is called. This ensures that the file IDs are created, allowing for the folders and files to be mapped appropriately.
-        /// </summary>
-        /// <param name="directoryPath"> The folder to add to the database. </param>
-        /// <param name="filePaths"> An array of all newly added file paths. </param>
-        /// <returns> A source folder DTO object for updating the UI. </returns>
-        public SourceFolderDto Add(string directoryPath, string[] filePaths)
-        {
-            SourceFolder entity = new SourceFolder
-            {
-                Path = directoryPath
-            };
-            _sourceFolderRepository.Add(entity);
-            _sourceFolderRepository.SaveChanges(); 
-            AddFolderFileMapping(filePaths, GetDirectoryId(directoryPath));
-
-            return new SourceFolderDto
-            {
-                Path = entity.Path,
-                Id = entity.Id
-            };
-        }
-
-        // This method adds the appropriate mapping. It stores the id of the monitored folder (directoryId).
-        // Then it stores the id for each file contained within that folder. The mapping can be used to know
-        // the exact files contained within a folder during its first addition to the database. So if files
-        // are added to that folder in the future then the program has a way to inform the user. 
-        private void AddFolderFileMapping(string[] filePaths, int directoryId)
-        {
-            List<SourceFile> filesInMonitoredFolder = GetMonitoredFolderChildrenFiles(filePaths);
-            foreach(SourceFile file in filesInMonitoredFolder)
-            {
-                _folderFileMappingRepository.Add(
-                    new FolderFileMapping
-                    {
-                        SourceFileId = file.Id,
-                        SourceFolderId = directoryId
-                    }
-                );
-            }
-            _folderFileMappingRepository.SaveChanges();
-        }
-
-        // Get all file paths from the database where the path matches the contents of "filePaths."
-        private List<SourceFile> GetMonitoredFolderChildrenFiles(string[] filePaths)
-            => _sourceFileRepository.GetRange(s => filePaths.Contains<string>(s.Path));
-
-        // Get the directory id (folder id) from the given directory path.
-        private int GetDirectoryId(string directoryPath) 
-            => _sourceFolderRepository.FirstOrDefault(f => f.Path == directoryPath).Id;
-        
-
-        /// <summary>
-        /// Remove a range of source folders from the database. This method also removes any source files contained within that folder, as long as the files were not added individually.
-        /// </summary>
-        /// <param name="ids"> The Ids for each source folder path to be removed. </param>
-        public void Remove(IEnumerable<int> ids)
-        {
-            foreach (int id in ids)
-            {
-                List<SourceFile> filesToRemove = GetFileEntitiesFromFolder(id);
-                _sourceFileRepository.RemoveRange(filesToRemove);
-                _sourceFileRepository.SaveChanges();
-
-                _sourceFolderRepository.Remove(new SourceFolder
-                { 
-                    Id = id 
-                });
-            }
-            _sourceFolderRepository.SaveChanges();
-        }
-
-        /// <summary>
-        /// Returns true if any monitored folder contains newly added files, false otherwise. 
-        /// </summary>
-        /// <param name="newFilesFromFolder"> An out parameter of type <see cref="Dictionary{TKey, TValue}"/> using <see cref="SourceFolderDto"/> objects for keys, and a list of strings for values. The values represent all files that have been added to the monitored folder. </param>
-        public bool FoldersContainNewFiles(
-            out Dictionary<SourceFolderDto, List<string>>? newFilesFromFolder)
-        {
-            List<SourceFolderDto> folders = GetFolders();
-
-            bool foldersAreUpdated = false;
-            newFilesFromFolder = null;
-            foreach (SourceFolderDto folder in folders)
-            {
-                string[] currentFiles = GetCurrentFilesFromFolder(folder);
-                List<string> storedFiles = GetFilePaths(GetFileEntitiesFromFolder(folder.Id));
-                foreach(string file in currentFiles)
-                {
-                    if (storedFiles.Contains(file)) continue;
-                    else
-                    {
-                        if(foldersAreUpdated == false) foldersAreUpdated = true;
-
-                        // Dictionary will be null on the first attempt.
-                        if (newFilesFromFolder == null)
-                            newFilesFromFolder = new Dictionary<SourceFolderDto, List<string>>();
-
-                        // If TryAdd returns true, add the key/value pair. Else, add the file to the currently existing key.
-                        if(!newFilesFromFolder.TryAdd(folder, new List<string>() { file }))
-                            newFilesFromFolder[folder].Add(file);
-                    }
-                }
-            }
-            return foldersAreUpdated;
-        }
-
-        // Get all current files and (including subdirectories) from the provided folder.
-        private string[] GetCurrentFilesFromFolder(SourceFolderDto folder) 
-            => Directory.GetFileSystemEntries(folder.Path, "*", SearchOption.AllDirectories);
-
-        /// <summary>
         /// Get all files stored in the database if they are mapped to a SourceFolder object. 
         /// </summary>
         /// <param name="folderId"> The ID of the folder to search in. </param>
@@ -180,6 +70,118 @@ namespace Services
             return result;
         }
 
+        /// <summary>
+        /// Adds a monitored folder to the database. This method ensures appropriate mapping from the source folder to all child files contained within it. <see cref="SourceFileService.Add(string)"/> must be called first on all new file paths before this method is called. This ensures that the file IDs are created, allowing for the folders and files to be mapped appropriately.
+        /// </summary>
+        /// <param name="directoryPath"> The folder to add to the database. </param>
+        /// <param name="filePaths"> An array of all newly added file paths. </param>
+        /// <returns> A source folder DTO object for updating the UI. </returns>
+        public SourceFolderDto Add(string directoryPath, string[] filePaths)
+        {
+            SourceFolder entity = new SourceFolder
+            {
+                Path = directoryPath
+            };
+            _sourceFolderRepository.Add(entity);
+            _sourceFolderRepository.SaveChanges(); 
+            AddFolderFileMapping(filePaths, GetDirectoryId(directoryPath));
+
+            return new SourceFolderDto
+            {
+                Path = entity.Path,
+                Id = entity.Id
+            };
+        }
+
+        /// <summary>
+        /// Remove a range of source folders from the database. This method also removes any source files contained within that folder, as long as the files were not added individually.
+        /// </summary>
+        /// <param name="ids"> The Ids for each source folder path to be removed. </param>
+        public void Remove(IEnumerable<int> ids)
+        {
+            foreach (int id in ids)
+            {
+                List<SourceFile> filesToRemove = GetFileEntitiesFromFolder(id);
+                _sourceFileRepository.RemoveRange(filesToRemove);
+                _sourceFileRepository.SaveChanges();
+
+                _sourceFolderRepository.Remove(new SourceFolder
+                {
+                    Id = id
+                });
+            }
+            _sourceFolderRepository.SaveChanges();
+        }
+
+        /// <summary>
+        /// Returns true if any monitored folder contains newly added files, false otherwise. If the method returns true, it will add the files to the database and provide an out parameter containing any files that were added to the database.
+        /// </summary>
+        /// <param name="newFilesFromFolder"> A list of data transfer objects for values. The values represent all files that have been added to the monitored folder. </param>
+        public bool FilesAddedToFolders(
+            out List<SourceFileDto>? newFilesFromFolder)
+        {
+            List<SourceFolderDto> folders = GetFolders();
+            bool filesAddedToFolders = false;
+            newFilesFromFolder = null;
+            foreach (SourceFolderDto folder in folders)
+            {
+                string[] currentFiles = GetCurrentFilesFromFolder(folder);
+                List<string> storedFiles = GetFilePaths(GetFileEntitiesFromFolder(folder.Id));
+                foreach (string file in currentFiles)
+                {
+                    if (storedFiles.Contains(file)) continue;
+                    else
+                    {
+                        SourceFileDto? sourceFile = AddFile(file);
+                        if (filesAddedToFolders == false) filesAddedToFolders = true;
+                        newFilesFromFolder.Add(sourceFile);
+                    }
+                }
+            }
+            return filesAddedToFolders;
+        }
+
+        private SourceFileDto? AddFile(string path)
+        {
+            // Add the file Entity to the database.
+            _sourceFileRepository.Add(
+                new SourceFile
+                {
+                    Path = path,
+                    Hash = EncryptionHelper.GetHash(path),
+                    IsModified = true
+                });
+            _sourceFileRepository.SaveChanges();
+            // Return the file as a data transfer object.
+            return _sourceFileRepository.FirstOrDefault(
+                f => f.Path == path,
+                f => new SourceFileDto
+                {
+                    Path = f.Path,
+                    Id = f.Id
+                });
+        }
+
+        // This method adds the appropriate mapping. It stores the id of the monitored folder (directoryId).
+        // Then it stores the id for each file contained within that folder. The mapping can be used to know
+        // the exact files contained within a folder during its first addition to the database. So if files
+        // are added to that folder in the future then the program has a way to inform the user. 
+        private void AddFolderFileMapping(string[] filePaths, int directoryId)
+        {
+            List<SourceFile> filesInMonitoredFolder = GetMonitoredFolderChildrenFiles(filePaths);
+            foreach(SourceFile file in filesInMonitoredFolder)
+            {
+                _folderFileMappingRepository.Add(
+                    new FolderFileMapping
+                    {
+                        SourceFileId = file.Id,
+                        SourceFolderId = directoryId
+                    }
+                );
+            }
+            _folderFileMappingRepository.SaveChanges();
+        }
+
         // Get all file Entities stored in the database if they are mapped to a SourceFolder object. 
         private List<SourceFile> GetFileEntitiesFromFolder(int folderId)
         {
@@ -191,13 +193,26 @@ namespace Services
                 SourceFile? file = _sourceFileRepository.FirstOrDefault(
                     f => f.Id == map.SourceFileId,
                     asNoTracking: false);
-                if(file != null) result.Add(file);
+                if (file != null) result.Add(file);
             }
             return result;
         }
 
+        // Get all file paths from the database where the path matches the contents of "filePaths."
+        private List<SourceFile> GetMonitoredFolderChildrenFiles(string[] filePaths)
+            => _sourceFileRepository.GetRange(s => filePaths.Contains<string>(s.Path));
+
+        // Get the directory id (folder id) from the given directory path.
+        private int GetDirectoryId(string directoryPath) 
+            => _sourceFolderRepository.FirstOrDefault(f => f.Path == directoryPath).Id;
+        
+
+        // Get all current files (including subdirectories) from the provided folder.
+        private string[] GetCurrentFilesFromFolder(SourceFolderDto folder)
+            => Directory.GetFileSystemEntries(folder.Path, "*", SearchOption.AllDirectories);
+
         // Get a list of the specified file paths from a SourceFile list.
-        private List<string> GetFilePaths(List<SourceFile> files)
+        private static List<string> GetFilePaths(List<SourceFile> files)
         {
             List<string> result = new List<string>();
             foreach (SourceFile file in files) result.Add(file.Path);
