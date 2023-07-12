1970bbfc76cf9d5384fa974f12c6e3b6c7ed070f
Alex <onegunnerunder@gmail.com>
2023-07-12 05:06:29 -0700

    Update code comments

--- Services/Dto/SourceFolderDto.cs
+++ Services/Dto/SourceFolderDto.cs
@@ -1,7 +1,7 @@
 ﻿namespace Services.Dto
 {
     /// <summary>
-    /// 
+    /// A data transfer object related to the SourceFolder Entity. Provides a full file path for any folder monitored by the program.
     /// </summary>
     public class SourceFolderDto
     {
